ccbdba9e66d2e1d511a66a8fec9cfbe2c10aed88
Дмитрий <dimanpacan2002@gmail.com>
2024-03-01 01:17:00 +0400

    Update: осталось доделать связи и с 11-16 миграцию проверить

---
+++ back/demo/src/main/java/ru/company/demo/entity/MaterialCharacteristics.java
@@ -0,0 +1,21 @@
+package ru.company.demo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "material_characteristics")
+public class MaterialCharacteristics extends AbstractEntity{
+
+
+
+}
