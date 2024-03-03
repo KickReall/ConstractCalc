6e99ed9525decd331a6b5d40f01789fb2bb2782e
godless007 <dimanpacan2002@gmail.com>
2024-03-03 23:58:50 +0400

    Update: подготовил всё для дальнейшей работы над Mvc и Spring Security

--- back/demo/src/main/java/ru/company/demo/CalculatorApplication.java
+++ back/demo/src/main/java/ru/company/demo/CalculatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
-	public static void main(String[] args) throws Throwable {
+	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
