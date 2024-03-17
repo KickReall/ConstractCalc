d8785ab06208da8aa476a4cdb34e3dae1cf2199c
Elena <helenbarskaya@mail.ru>
2024-03-17 12:09:50 +0400

    Added CORS

--- back/demo/src/main/java/ru/company/demo/rest/CalculationController.java
+++ back/demo/src/main/java/ru/company/demo/rest/CalculationController.java
@@ -1,13 +1,11 @@
 package ru.company.demo.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.company.demo.business.service.CalculationService;
 import ru.company.demo.domain.entity.Calculation;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/calculation")
